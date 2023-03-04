22565ebd57465c2211a1af554a34894eeb303ef1
Igneyy <igney64@gmail.com>
2023-03-03 23:47:36 -0500

    publish question pass

--- src/objects.ts
+++ src/objects.ts
@@ -107,7 +107,9 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    const publish = !question.published;
+    const publishedQuestion = { ...question, published: publish };
+    return publishedQuestion;
 }
 
 /**
