f8bf8683c2aa00b88b3a262866aa8fdc3ddd5ad4
Igneyy <igney64@gmail.com>
2023-03-08 22:47:48 -0500

    getPublished pass

--- src/nested.ts
+++ src/nested.ts
@@ -6,7 +6,10 @@ import { Question, QuestionType } from "./interfaces/question";
  * that are `published`.
  */
 export function getPublishedQuestions(questions: Question[]): Question[] {
-    return [];
+    const publishedQuestions = questions.filter(
+        (aQuestion: Question): Question => aQuestion.published === true
+    );
+    return publishedQuestions;
 }
 
 /**
