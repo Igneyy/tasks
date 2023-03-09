d186447d4f86a6be8ef311a73a7724b8bb23a9dc
Igneyy <igney64@gmail.com>
2023-03-08 23:18:20 -0500

    sumPublishedPoints pass

--- src/nested.ts
+++ src/nested.ts
@@ -82,7 +82,14 @@ export function sumPoints(questions: Question[]): number {
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    const summedPoints = questions.reduce(
+        (pointsTotal: number, aQuestion: Question) =>
+            aQuestion.published === true
+                ? pointsTotal + aQuestion.points
+                : pointsTotal,
+        0
+    );
+    return summedPoints;
 }
 
 /***
