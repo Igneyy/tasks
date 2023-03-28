c2288f64d1eea2dfe088bc94695b2022bb0a888d
Igneyy <igney64@gmail.com>
2023-03-28 07:57:36 -0400

    multiple choice questions pass

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,36 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [currentlySelected, setCurrentlySelected] = useState<string>(
+        options[0]
+    );
+
+    function updateSelected(event: React.ChangeEvent<HTMLSelectElement>) {
+        setCurrentlySelected(event.target.value);
+    }
+
     return (
         <div>
-            <h3>Multiple Choice Question</h3>
+            <h3>
+                <Form.Group>
+                    <Form.Label>Multiple Choice Question</Form.Label>
+                    <Form.Select
+                        value={currentlySelected}
+                        onChange={updateSelected}
+                    >
+                        {options.map((color: string) => (
+                            <option key={color} value={color}>
+                                {color}
+                            </option>
+                        ))}
+                    </Form.Select>
+                </Form.Group>
+                {currentlySelected === expectedAnswer ? (
+                    <span>✔️</span>
+                ) : (
+                    <span>❌</span>
+                )}
+            </h3>
         </div>
     );
 }
