f652fc0995ea9ffefc193313283f70b3c48163cd
Igneyy <igney64@gmail.com>
2023-03-28 06:55:28 -0400

    check answer, edit mode, and give attempts

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,38 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [numAttempts, setNumAttempts] = useState<number>(3);
+    const [applyAttempts, setApplyAttempts] = useState<string>("0");
+    const applyAtt = parseInt(applyAttempts) || 0;
+
+    function subtractOne(): void {
+        setNumAttempts(numAttempts - 1);
+    }
+
+    function addAttempts(): void {
+        setNumAttempts(numAttempts + applyAtt);
+    }
+
     return (
         <div>
-            <h3>Give Attempts</h3>
+            <h3>
+                <Button onClick={subtractOne} disabled={numAttempts <= 0}>
+                    Use
+                </Button>
+                <Button onClick={addAttempts}>Gain</Button>
+                <Form.Group controlId="GiveAttempts">
+                    <Form.Label>Request Attempts:</Form.Label>
+                    <Form.Control
+                        type="number"
+                        value={applyAttempts}
+                        onChange={(
+                            event: React.ChangeEvent<HTMLInputElement>
+                        ) => setApplyAttempts(event.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+                <span>Number of attempts: {numAttempts}</span>
+            </h3>
         </div>
     );
 }
